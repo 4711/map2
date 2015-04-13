793cfd296da3d893b080e38fdd5239248c4c15d1
4711 <bzthcyqullt@mailinator.com>
2015-04-13 18:01:01 +0200

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -142,12 +142,12 @@ var MapsLib = {
   //-----custom filters for point data layer
     //---MODIFY column header and values below to match your Google Fusion Table AND index.html
     //-- TEXTUAL OPTION to display legend and filter by non-numerical data in your table
-    var type_column = "'Program Type'";  // -- note use of single & double quotes for two-word column header
+    var type_column = "'speed'";  // -- note use of single & double quotes for two-word column header
     var tempWhereClause = [];
     if ( $("#cbType1").is(':checked')) tempWhereClause.push("Interdistrict");
     if ( $("#cbType2").is(':checked')) tempWhereClause.push("District");
     if ( $("#cbType3").is(':checked')) tempWhereClause.push("MorePreK");
-    whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
+    //whereClause += " AND " + type_column + " IN ('" + tempWhereClause.join("','") + "')";
 
     //-- NUMERICAL OPTION - to display and filter a column of numerical data in your table, use this instead
     /*    var type_column = "'TypeNum'";
